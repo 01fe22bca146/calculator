47976ebdc4ba54a0a1c5b0d0ff4aefa1d5ed7201
Nayeempatwegar <157590898+Nayeempatwegar@users.noreply.github.com>
2024-01-24 16:18:30 +0530

    Add files via upload

---
+++ calc.java
@@ -0,0 +1,12 @@
+class calc{
+
+	 float getSquare(float x) {
+		return x * x;
+	}
+    float getcube(float x){
+        return x*x*x;
+    }
+    float getmod(float x,float y){
+        return y%x;
+    }
+}
---
+++ calculator.java
@@ -0,0 +1,41 @@
+class calculator extends calc {
+float add(float x, float y) {
+return x + y;
+}
+
+float sub(float x, float y) {
+return x - y;
+}
+
+float mul(float x, float y) {
+return x * y;
+}
+
+float div(float x, float y) {
+return x / y;
+}
+
+public static void main(String[] args) {
+calculator c = new calculator();
+float x = 10;
+float y = 20;
+
+float add = c.add(x, y);
+System.out.println(add);
+
+float sub = c.sub(x, y);
+System.out.println(sub);
+
+float mul = c.mul(x, y);
+System.out.println(mul);
+
+float div = c.div(x, y);
+System.out.println(div);
+
+float square=c.getSquare(x);
+System.out.println(square);
+
+float cube=c.getcube(x);
+System.out.println(cube);
+}
+}
